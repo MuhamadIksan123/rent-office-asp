8fc7c1c6969fd9a7b2fe7ef9c9d7fe134a6dd9b2
Muhamad Iksan <muhamadiksan351@gmail.com>
2025-02-01 15:15:48 +0700

    Fix line endings

--- Models/OfficeSpaceBenefit.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RentOffice.Models
-{
-    public class OfficeSpaceBenefit
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public DateOnly CreatedAt { get; set; }
-    }
-}
--- Models/OfficeSpacePhoto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RentOffice.Models
-{
-    public class OfficeSpacePhoto
-    {
-        public int Id { get; set; }
-        public string Photo { get; set; }
-        public DateOnly CreatedAt { get; set; }
-    }
-}
